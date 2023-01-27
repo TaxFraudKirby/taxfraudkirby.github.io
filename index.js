f64b782a6e11e71788c0bf8edf6bb6db60f28a75
tax fraud kirby <123656016+TaxFraudKirby@users.noreply.github.com>
2023-01-27 11:36:50 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function setup() {
+  new Canvas(windowWidth, windowHeight); // Do not remove
+  // This code will run once at startup
+}
+
+function draw() {
+  // This code will run forever
+}
+
+// Documentation available at: https://p5play.org/docs/
+// Code.org documentation available at: https://studio.code.org/docs/ide/gamelab
+// Examples available at: https://p5play.org/demos/
